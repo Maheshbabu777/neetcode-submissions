950f516f3d84594162082b84f5bfbb8a0cc93bd2
Maheshbabu <146876563+Maheshbabu777@users.noreply.github.com>
2026-06-23 20:35:49 +0530

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.java
@@ -0,0 +1,49 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int minMeetingRooms(List<Interval> list) {
+        // int n=intervals.size();
+        // if(n==0)return 0;
+        // Collections.sort(intervals,Comparator.comparingInt(a->a.start));
+        // List<List<Interval>> li=new ArrayList<>();
+        // li.add(new ArrayList<>(List.of(intervals.get(0))));
+        // for(int i=1;i<n;i++){
+        //     Interval a=intervals.get(i);
+        //     int st=a.start;
+        //     boolean can=false;
+        //     for(List<Interval> temp:li){
+        //         if(temp.get(0).end<=st){
+        //             temp.add(0,a);
+        //             can=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!can){
+        //         li.add(new ArrayList<>(){{
+        //             add(a);
+        //         }});
+        //     }
+        // }
+        // return li.size();  
+
+        list.sort((a,b)->a.start-b.start);
+        PriorityQueue<Integer> minH=new PriorityQueue<>();
+        for(Interval in:list){
+            if(!minH.isEmpty() && minH.peek()<=in.start){
+                minH.poll();
+            }
+            minH.offer(in.end);
+        }
+        return minH.size();
+            
+    }
+}
